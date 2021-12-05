fc4ff980ebdb201a6dba73acb54bfc0b60204483
Highfire1 <tseng.andersonn@gmail.com>
2021-12-04 18:06:34 -0800

    ugh

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,30 @@
 
     const ui = require("ui-lib/library");
     
-    ui.addButton("thorium-grabber", Items.thorium, () => {
-        const core = Vars.state.teams.cores(Vars.player.team).first().tile;
-        Call.requestItem(Vars.player, core, Items.thorium, 15);
+    ui.addButton("function", Items.thorium, () => {
+        log("HELLO?!?!?!?")
+        
+        for(planet in Vars.content.planets) {
+            for(sec in planet) {
+                log(sec) // doesn't do anything idk why
+                log(sec.id.toString()) // doesn't do anything idk why
+
+                sec.clearInfo()
+                if(sec.save != null){
+                    sec.save.delete();
+                    sec.save = null;
+                }
+            }
+        }
+        
+        
+        for(slot in Vars.control.saves.getSaveSlots().copy()){
+            if(slot.isSector()){
+                slot.delete();
+            }
+        }
+
+        log("HELLO?!?!?!?")
     });
     
-    })();
+})();
